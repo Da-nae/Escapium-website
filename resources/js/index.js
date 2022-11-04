188811cb41be7b20cdd85beb06a2660d129b8cf1
Da-nae <grosjean.danae@gmail.com>
2022-11-04 15:35:34 +0100

    first try faq accordeon

--- resources/js/index.js
+++ resources/js/index.js
@@ -28,3 +28,36 @@ const navLinks = document.querySelector(".navbar__navigation")
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 });
+
+/* Accordion for FAQ */
+
+const faqAll = document.querySelectorAll(".faq__content");
+
+const openFaq = (accordeon) => {
+    const content = accordeon.querySelector(".faq__accordeon");
+    accordeon.classList.add("accordeon__active");
+    content.style.opacity = 1;
+    content.style.height = 50;
+}
+
+const closeFaq = (accordeon) => {
+    const content = accordeon.querySelector(".faq__accordeon");
+    accordeon.classList.remove("faq__accordeon");
+    content.style.opacity = 0;
+    content.style.height = 0;
+}
+
+faqAll.forEach((accordeon) => {
+    const button = accordeon.querySelector(".faq__button");
+	const content = accordeon.querySelector(".faq__accordeon");
+
+    button.onclick = () => {
+		if (content.style.opacity == 1) {
+			closeFaq(accordeon);
+		} else if (content.style.opacity == 0) {
+            faqAll.forEach((accordeon) => closeFaq(accordeon));
+			openFaq(accordeon);
+		}
+	};
+})
+
